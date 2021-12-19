76c023eb0a4f9e3155ca3f36a22b4743d42353b9
Svetlana Mikisheva <svetamikis@yandex.ru>
2021-12-19 16:00:33 +0500

    second commit

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -17,3 +17,21 @@ test('should return sort health', () => {
 
   expect(received).toEqual(expected);
 });
+
+test('should not toBe', () => {
+  const input = [
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+  ];
+
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  const received = sortHealth(input);
+
+  expect(received).not.toBe(expected);
+});
